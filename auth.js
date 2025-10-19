8a75a6ebc010ad9fc18dd9c5e03dab709d7bf202
Puskesmas Padasua <schdotco@gmail.com>
2025-10-19 12:49:29 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,33 @@
-// js/auth.js
-import { loginUser } from './app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDciAiNKFbh8aoM8joIywSeH9Boml1tK5s",
+  authDomain: "aplikasi-jadwal-1d54d.firebaseapp.com",
+  databaseURL: "https://aplikasi-jadwal-1d54d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aplikasi-jadwal-1d54d",
+  storageBucket: "aplikasi-jadwal-1d54d.firebasestorage.app",
+  messagingSenderId: "860941812271",
+  appId: "1:860941812271:web:458bd63c5b0d2514fcbcda"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// LOGIN FORM
 const loginForm = document.getElementById('loginForm');
 if (loginForm) {
   loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
     try {
-      await loginUser(email, password);
-      window.location.href = 'dashboard.html';
-    } catch (err) {
-      alert('Login gagal: ' + err.message);
+      await signInWithEmailAndPassword(auth, email, password);
+      alert('✅ Login berhasil');
+      window.location.href = "dashboard.html";
+    } catch (error) {
+      alert('❌ Login gagal: ' + error.message);
     }
   });
 }
