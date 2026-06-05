a4fa4694e36257abfe652e0b233c013db4ffb1e4
Puskesmas Padasua <schdotco@gmail.com>
2026-06-05 14:02:13 +0700

    Update inputckg.js

--- inputckg.js
+++ inputckg.js
@@ -13,15 +13,6 @@
 (function(){
 'use strict';
 
-   /* ================= PENCEGAHAN ERROR LAUNCHER ================= */
-    // Menangkap jembatan GM_xmlhttpRequest dari Launcher
-    const fetchAPI = typeof GM_xmlhttpRequest !== "undefined" ? GM_xmlhttpRequest : window.GM_xmlhttpRequest;
-
-    if (!fetchAPI) {
-        alert("CRITICAL ERROR: Fungsi fetchAPI tidak terdeteksi! Script tidak bisa menarik data.");
-        return;
-    }
-
 /* =========================================================
    CONFIG
 ========================================================= */
