bc7cc09fc4ec7df1095d7a4e19bd7ec7cf16cadf
Puskesmas Padasua <schdotco@gmail.com>
2026-06-07 10:57:02 +0700

    Update regckg.js

--- regckg.js
+++ regckg.js
@@ -13,10 +13,10 @@ const SHEETS = [{
     colTgl: 8,
     colWA: 20,
     colJK: 6,
-    colPekerjaan: 25,
+    colPekerjaan: 22,
     colKelurahan: 17,
     colAlamat: 14,
-    colMartial: 27,
+    colMartial: 21,
     waStatis: true
 }];
 
