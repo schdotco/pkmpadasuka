3f17fad98bb0731c314f8b3d4283f318aa4bcf74
Puskesmas Padasua <schdotco@gmail.com>
2026-06-21 10:25:22 +0700

    Update skriningckg.js

--- skriningckg.js
+++ skriningckg.js
@@ -828,6 +828,13 @@ async function mainLoop(data){
     while (BOT_RUNNING && location.hostname.includes('sehatindonesiaku')) {
         const nextItem = getNextTarget();
 
+        for (let i = 0; i < 3; i++) {
+            nextItem = getNextTarget();
+            if (nextItem) break; 
+            console.log("Tombol belum muncul, mencoba lagi dalam 2 detik...");
+            await sleep(2000);
+        }
+
         if(!nextItem){
             BOT_RUNNING = false;
         
@@ -840,7 +847,7 @@ async function mainLoop(data){
 
         updateStatus('MEMBUKA TARGET:\n' + nextItem.title.toUpperCase());
         addCompleted(nextItem.id); // Tandai sudah diklik
-        await sleep(2000);
+        await sleep(15000);
         nextItem.btn.click();
         await sleep(4000);
     }
